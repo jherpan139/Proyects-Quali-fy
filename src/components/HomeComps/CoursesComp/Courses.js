f2e3f8317ba53379970986cc491a8a8c133e299c
Jorge <jherpan77@iesalfonsoxi.com>
2022-06-15 00:12:46 +0200

    Table next

--- src/components/HomeComps/CoursesComp/Courses.js
+++ src/components/HomeComps/CoursesComp/Courses.js
@@ -10,6 +10,9 @@ import Delete from '@mui/icons-material/DeleteRounded'
 import Modify from '@mui/icons-material/ChangeCircleRounded'
 import getTeachersOfCourses from '../../../request/getTeachersOfCourses'
 import CreateCourseModal from './CourseActions/CreateCourse'
+import BootstrapTable from 'react-bootstrap-table-next'
+import StudentsModal from './ModalStudents'
+import paginationFactory from 'react-bootstrap-table2-paginator';
 
 const AdminPanel = (props) => {
   return (
@@ -45,36 +48,19 @@ const Courses = () => {
   const [coursesList, setCoursesList] = React.useState([])
   const [createCourseModal, setCreateCourseModal] = React.useState(false)
   const [isLoading, setLoading] = React.useState(true)
+  const [showModal, setShowModal] = React.useState(false)
+  const [modalInfo, setModalInfo] = React.useState([])
+  const [show, setShow] = React.useState(false)
+
+  const handleClose = () => setShow(false)
+  const handleOpen = () => setShow(true)
 
   React.useEffect(() => {
     listAllCourses(user.token)
     setLoading(false)
   },[])
 
-  if (isLoading){
-  return (
-    <Table className="table table-dark">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">Curso</th>
-          <th scope="col">Alumnos</th>
-          <th scope="col">Profesores</th>
-        </tr>
-      </thead>
-      <tbody>
-      <tr>
-          <td scope="row"><Cached></Cached></td>
-          <td><Cached></Cached></td>
-          <td><Cached></Cached></td>
-          <td><Cached></Cached></td>
-        </tr>
-      </tbody>
-    </Table>
-  )
-  }
-  return (
-    <>
+  /**    <>
     <Table striped bordered hover variant="dark" style={{align:'center'}}>
       <thead>
         <tr>
@@ -104,6 +90,91 @@ const Courses = () => {
         token={user.token}
     >
     </CreateCourseModal>
+</> */
+
+const columns=[{
+  dataField:"idCourse",
+  text:"#",
+  sort: true
+},
+{
+  dataField:"name",
+  text:"Curso",
+  sort: true
+},
+{
+  text:"Estudiantes",
+  formatter: studentsFormatter
+},
+{
+  text:"Profesores",
+  formatter: teachersFormatter
+}
+]
+
+const rowEvents = {
+  onClick: (e, row) => {
+    setModalInfo(row)
+    toggleTrueFalse()
+  }
+}
+
+const toggleTrueFalse = () => {
+  setShowModal(handleOpen)
+}
+
+function studentsFormatter(cell, row) {
+  if (row.students.length>0) {
+    return (
+      <>
+        <p>{row.students.length}</p>
+      </>
+    )
+  } else {
+    return (
+      <p>No tiene estudiantes asignados</p>  
+    )
+  }
+}
+
+function teachersFormatter(cell, row) {
+  if (row.users.length>0) {
+    return (
+      <Button>Profesores</Button>
+    )
+  } else {
+    return (
+      <Button disabled>Profesores</Button>
+    )
+  }
+}
+
+  return (
+    <>
+      <BootstrapTable
+      classes="table-dark"
+      keyField='idCourse'
+      data={coursesList}
+      columns={columns}
+      striped
+      rowEvents={rowEvents}
+      caption={<h1 className='text-center'>LISTADO DE CURSOS</h1>}
+      pagination={paginationFactory()}
+      >
+      </BootstrapTable>
+      {show ? <StudentsModal show={show} onHide={handleClose} coursename={modalInfo.name} students={modalInfo.students}/> : null}
+      <Table className='table table-dark'>
+        <tbody>
+        {user && user.role === 'ADMIN' ? <AdminPanel setCreateCourseModal={setCreateCourseModal}></AdminPanel> : null}
+        </tbody>
+      </Table>
+      <CreateCourseModal
+        show={createCourseModal}
+        onHide={() => setCreateCourseModal(false)}
+        token={user.token}
+      >
+      </CreateCourseModal>
+
 </>
   )
 }
